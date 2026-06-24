cb471e287a4b84441ea0d5f4ca00dc666c25a462
heavenly-sword <heavenlysword@hotmail.com>
2026-06-24 12:20:58 +0800

    Add progress bar and computation placeholder to enhance user feedback

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import time
 
 if st.sidebar.checkbox('Show Uber pickups in NYC'):
     st.title('Uber pickups in NYC')
@@ -104,3 +105,17 @@ with right_column:
         'Sorting hat',
         ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
     st.write(f"You are in {chosen} house!")
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
