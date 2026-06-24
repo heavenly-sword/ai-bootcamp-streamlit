119984c775bf66c41eb9080b14395d2254a78de7
heavenly-sword <heavenlysword@hotmail.com>
2026-06-24 12:17:48 +0800

    Add sidebar selectbox and slider for user interaction options

--- uber_pickups.py
+++ uber_pickups.py
@@ -81,3 +81,15 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
