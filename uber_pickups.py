533f903078434f7d62d9af5647f2980f0e095807
heavenly-sword <heavenlysword@hotmail.com>
2026-06-24 12:18:55 +0800

    Refactor checkbox to sidebar for better UI organization

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-if st.checkbox('Show Uber pickups in NYC'):
+if st.sidebar.checkbox('Show Uber pickups in NYC'):
     st.title('Uber pickups in NYC')
 
     DATE_COLUMN = 'date/time'
